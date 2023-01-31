49972751a4668af2d0a81dd291194d5104388092
Lauren <halpertln@gmail.com>
2023-01-31 10:30:54 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+    return Math.abs(42-destination);
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(destination) {
+    const feet = distanceFromHqInBlocks(destination) * 264;
+    return feet;
+
+}
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination) {
+    const blocksToFeet = Math.abs(start - destination) * 264;
+    return blocksToFeet;
+}
+distanceTravelledInFeet();
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance >= 2500){
+        return `cannot travel that far`
+    }
+    if (distance >= 2000 && distance < 2500) {
+        return 25;
+    }
+    if (distance <= 400) {
+        return 0;
+    }
+    return (distance - 400) * .02;
+}
+calculatesFarePrice();
